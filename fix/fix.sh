c39162b1bbf03e3d6cf3b1c2b43cede62aa4e87e
RifkyStoretuneling <mhmdrifqim@gmail.com>
2024-06-08 20:07:40 +0700

    Update fix.sh

--- fix/fix.sh
+++ fix/fix.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
-rm -f ${0}
+mkdir /root/backup
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cp -r /etc/xray backup/xray
+cp -r /var/www/html/ backup/html
+cd /root
+zip -r backup.zip backup > /dev/null 2>&1
 
-echo " Prosess Fix backup server"
-
-apt install rclone -y
-printf "q\n" | rclone config
-wget -q -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/zhets/ScriptAutoInstall-xdxl/main/rclone.conf"
-cd /bin
-git clone  https://github.com/zhets/wondershaper.git
-cd wondershaper
-sudo make install
-cd
-rm -rf wondershaper
-echo > /home/limit
-apt install msmtp-mta ca-certificates bsd-mailx -y
-cat<<EOF>>/etc/msmtprc
-defaults
-tls on
-tls_starttls on
-tls_trust_file /etc/ssl/certs/ca-certificates.crt
-account default
-host smtp.gmail.com
-port 587
-auth on
-user oceantestdigital@gmail.com
-from oceantestdigital@gmail.com
-password jokerman77
-logfile ~/.msmtp.log
-EOF
-chown -R www-data:www-data /etc/msmtprc
-
-sleep 1
-clear
-echo "Done"
+rclone copy backup.zip dr:backup/
+url=$(rclone link dr:backup/backup.zip)
+id=(`echo $url | grep '^https' | cut -d'=' -f2`)
+linkbackup="https://drive.google.com/u/4/uc?id=${id}&export=download"
+echo $linkbackup >/tmp/link
+rm -r backup && rm backup.zip
+echo " Your link backup : "
+echo "${linkbackup}"
